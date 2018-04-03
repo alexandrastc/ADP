57d07dd73dac9ffb583759ce9a62726efaf8dd74
Alexandra <alexandrastc09@gmail.com>
2018-04-03 18:20:29 +0100

    Fixed issue, sending chunks now

--- LAB 4/mpi.py
+++ LAB 4/mpi.py
@@ -1,24 +1,27 @@
 from mpi4py import MPI
 import random
+import math
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 numproc = comm.Get_size()
 partial_sum = 0
+rand_numbers = []
+
+for i in range(0, numproc*random.randint(1,5)):
+    rand_numbers.append(random.randint(1, 5))
+
+chunksize = int(math.ceil(len(rand_numbers)/numproc))
 
 if rank == 0:
 
-    rand_numbers = []
-
-    for i in range(0, numproc):
-        rand_numbers.append(random.randint(1, 5))
-
     print("my numbers are = ", rand_numbers)
-    partial_sum = rand_numbers[0]
-    print("process ", rank, " got number ", partial_sum)
+
+    for element in range(rank*chunksize, rank*chunksize+chunksize):
+        partial_sum += rand_numbers[element]
 
     for i in range(1, numproc):
-        req = comm.isend(rand_numbers, dest=i, tag=0)
+        req = comm.isend(rand_numbers[i*chunksize:i*chunksize+chunksize:1], dest=i, tag=0)
         req.wait()
 
     for i in range(1, numproc):
@@ -28,10 +31,12 @@ if rank == 0:
     print("sum = ", partial_sum)
 
 else:
+
     req = comm.irecv(source=0, tag=0)
     rand_numbers = req.wait()
-    partial_sum = rand_numbers[rank]
-    print("process ", rank, " got number ", partial_sum)
+
+    for number in rand_numbers:
+        partial_sum += number
 
     req2 = comm.isend(partial_sum, dest=0, tag=rank)
     req2.wait()
